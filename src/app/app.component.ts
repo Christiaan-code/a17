64330f3bcb88a8239760abc881a6d80a062b656f
Christiaan du Preez <christiaan.d@jobjack.co.za>
2024-03-18 10:21:07 +0200

    Setup a card and loading spinners with a potential loading/defer pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Signal, computed } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { MyService } from './my.service'
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component'
+import { CustomCardComponent } from './custom-card/custom-card.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingSpinnerComponent, CustomCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'a17';
+  constructor(private myService: MyService) {}
+  title: string = 'a17'
+
+  items1: Signal<number[] | null> = this.myService.items1
+  items2: Signal<number[] | null> = this.myService.items2
+
+  loaded1: Signal<boolean> = computed(() => {
+    return this.items1() !== null
+  })
+
+  loaded2: Signal<boolean> = computed(() => {
+    return this.items2() !== null
+  })
 }
---
+++ src/app/my.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Signal } from '@angular/core'
+import { of, delay, Observable } from 'rxjs'
+import { toSignal } from '@angular/core/rxjs-interop'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyService {
+  constructor() {}
+
+  items1: Signal<number[] | null> = toSignal(this.getItems1(), { initialValue: null })
+  items2: Signal<number[] | null> = toSignal(this.getItems2(), { initialValue: null })
+
+  getItems1(): Observable<number[]> {
+    return of([1,2,3,4,5]).pipe(delay(3000))
+  }
+
+  getItems2(): Observable<number[]> {
+    return of([]).pipe(delay(3000))
+  }
+}
